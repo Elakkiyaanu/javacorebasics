8ef4ddc4873e6e035f1c4c40179ae143535b7f09
Elakkiyaanu <elakkiyalea@gmail.com>
2018-06-05 09:56:23 +0530

    Arrays examples

---
+++ src/com/basic/SingleArraySimple.java
@@ -0,0 +1,23 @@
+package com.basic;
+
+public class SingleArraySimple {
+	public static void main(String[] args) {
+		String names[];
+		names= new String[6];
+		
+		
+		names[0]="Anu";
+		names[1]="Janu";
+		names[2]="nisha";
+		names[3]="yuva";
+		names[4]="Akal";
+		names[5]="logi";
+		        System.out.println(names[0]);
+		        System.out.println(names[1]);
+		        System.out.println(names[2]);
+				System.out.println(names[3]);
+			    System.out.println(names[4]);
+	            System.out.println(names[5]);
+
+}
+}
---
+++ src/com/basic/SingleArraySimple1.java
@@ -0,0 +1,11 @@
+package com.basic;
+
+public class SingleArraySimple1 {
+	public static void main(String[] args) {
+		
+		String names[]= { "Raja" ,"Anu" , "Kavin" ,"Nisha" , "Akal" ,"logi" };
+		for(int i=0; i < names.length; i++)
+			System.out.println(names[i]);
+	}
+
+}
