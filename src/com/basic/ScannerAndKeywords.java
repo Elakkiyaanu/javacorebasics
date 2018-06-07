2b649f2b2f970a6e2c94e1e446870135960c60f3
Elakkiyaanu <elakkiyalea@gmail.com>
2018-06-07 10:30:41 +0530

    scanner example

---
+++ src/com/basic/ScannerAndKeywords.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+import java.util.Scanner;
+
+public class ScannerAndKeywords {
+
+
+	
+
+	  public static void main(String[] args)
+	  { 
+		  Scanner s = new Scanner(System.in);
+	    System.out.print( "Enter your name: "  );
+	    String name = s.nextLine();
+	    System.out.println( "Hello " + name + "!" );
+	  }
+	}
+
