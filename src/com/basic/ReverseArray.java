ad061b17b006d57264a19ec36850c1c9bdf054cc
Elakkiyaanu <elakkiyalea@gmail.com>
2018-06-07 10:31:41 +0530

    value reverse examples

---
+++ src/com/basic/ReverseArray.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class ReverseArray {
+	public static void main(String args[])
+	{
+int myarray[]={ 9,7,99,77,97,79};
+for(int counter=myarray.length-1;counter>=0;counter--) {
+	
+System.out.println(myarray[counter]);
+
+	}
+	
+}
+}
