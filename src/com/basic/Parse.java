c3cd67449badb7866026b72197786798861adb7d
Elakkiyaanu <elakkiyalea@gmail.com>
2018-06-08 18:54:47 +0530

    Parse examples

---
+++ src/com/basic/Parse.java
@@ -0,0 +1,15 @@
+
+package com.basic;
+
+public class Parse {
+	public static void main(String args[])
+	{
+		double dn1=Double.parseDouble(args[0]);
+		double dn2=Double.parseDouble(args[1]);
+		 double sum = dn1+dn2;
+		System.out.println("value of"+sum);
+		
+	}
+
+
+}
