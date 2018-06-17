3cfc3ef5113387645c4815062d2b4d7e1aeddb80
Elakkiyaanu <elakkiyalea@gmail.com>
2018-06-17 15:43:10 +0530

    Inheritance Examples

---
+++ src/com/basic/Inheritance_demo.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class Inheritance_demo {
+	  public static void  main(String args[])
+
+	    {
+
+	        A obj = new A();
+
+	        obj.i=1;
+
+	        obj.j=2;   
+
+	        obj.display();     
+
+	    }
+}
