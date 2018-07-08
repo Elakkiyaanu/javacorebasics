c06ab8a43325e1f7727b580c2c8c957450bb943d
Elakkiyaanu <elakkiyalea@gmail.com>
2018-07-08 10:04:44 +0530

    SynchronizedWorker Example

---
+++ src/com/basic/SynchronizedWorker.java
@@ -0,0 +1,60 @@
+package com.basic;
+
+public class SynchronizedWorker {
+	private int count =0;
+	public static void main(String args[]) {
+		SynchronizedWorker worker = new SynchronizedWorker();
+		worker.doWork();
+		
+	}
+	public void increment() throws InterruptedException
+	{
+		count++;
+		
+	}
+	public void doWork()
+	{
+		Thread thread1= new Thread(new Runnable())
+				{
+			public void run()
+			{
+			for(int i=0;i< 10000;i++)
+			{
+				try {
+					increment();
+					
+				}
+				catch (InterruptedException ex)
+			{
+				ex.printStackTrace();
+			}
+				}
+			}
+				}
+	}
+	thread1.start();
+	
+	Thread thread2 = new Thread(new Runnable()) {
+		
+			public void run()
+			{
+				for(int i=0;i<10000;i++)
+				{
+					try {
+						increment();
+						
+					}catch (InterruptedException ex) {
+						ex.printStackTrace();
+					}
+				}
+			}
+	
+			
+		
+	try {
+		thread1.join();
+		thread2.join();
+	}catch(InterruptedException ignored) {}
+	System.out.println("Count is:" + count);
+}
+	
