16bf81689b8c1f11d50f71bc79bbfb59fbb4eb72
Elakkiyaanu <elakkiyalea@gmail.com>
2018-07-08 09:29:02 +0530

    Concate Example

---
+++ src/com/string/ConcateExample.java
@@ -0,0 +1,9 @@
+package com.string;
+
+public class ConcateExample {
+	
+		public static void main(String args[]){
+		String s1="hello";
+		s1=s1.concat("how are you");
+		System.out.println(s1);
+		}}
