98cc1b27a29891895dde2c23ce90fac70a640143
Elakkiyaanu <elakkiyalea@gmail.com>
2018-07-08 09:33:28 +0530

    Word Form Example

---
+++ src/com/logical/FormPrime.java
@@ -0,0 +1,31 @@
+package com.logical;
+import java.util.Scanner;
+
+public class FormPrime {
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the word");
+		String str=scan.next();
+		char rev=0;
+	
+		//int text=str.length();
+	//	char ch=str.to CharArray();
+	
+		for(int i=0;i<str.length();i++) {
+			for(int j=((str.length())-1);j>i;j--) {
+		
+			//char first=str.charAt(0);
+			//char last=str.charAt(str.length()-1);
+				if(i==j) {
+					// String[] a;
+					rev = (char) j; 
+					
+					}
+			}
+		}
+		}
+	}
+
+		
+		
+	
